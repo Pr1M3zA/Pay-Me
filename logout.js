92416e23c15cc812dfd6a85bf4f1b21c1b11e6c1
Pr1M3zA <priscilav.mezal@gmail.com>
2024-12-08 22:31:54 -0700

    Updated web page

--- logout.js
+++ logout.js
@@ -1,13 +1,22 @@
-/*document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.getElementById('logout');
     if (logoutButton) {
         logoutButton.addEventListener('click', () => {
-            sessionStorage.removeItem("token");  
-            window.location.href = '../home_page/home_page.html';
+            Swal.fire({
+                title: "Confirmar",
+                text: "¿Estás seguro que quieres cerrar sesión?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Confirmar",
+                cancelButtonText: "Cancelar"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    sessionStorage.removeItem("token");
+                    window.location.href = '../home_page/home_page.html';
+                }
+            });
         });
     }
 });
-*/
-
-sessionStorage.removeItem("token");  
-window.location.href = '../home_page/home_page.html';
